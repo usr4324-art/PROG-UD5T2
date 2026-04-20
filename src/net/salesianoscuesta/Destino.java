3333fd85669fcc6b2ef06557b25fe956cdc9f63c
LujánEspinoza <usr4324@salesianos-lacuesta.net>
2026-04-20 10:16:49 +0100

    añadidos constructor, getters y setters más override

--- src/net/salesianoscuesta/Destino.java
+++ src/net/salesianoscuesta/Destino.java
@@ -1,8 +1,37 @@
 package net.salesianoscuesta;
 
 public class Destino {
-        private String nombreDestino;
-        private String pais;
-        private String tipoAlojamiento;
-        private int puntuacion;
+    private String nombreDestino;
+    private String pais;
+    private String tipoAlojamiento;
+    private int puntuacion;
+
+    public Destino(String nombre, String pais, String tipo, int puntuacion) {
+        this.nombreDestino = nombre;
+        this.pais = pais;
+        this.tipoAlojamiento = tipo;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre() {
+        return nombreDestino;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getTipo() {
+        return tipoAlojamiento;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + puntuacion + "★] " + nombreDestino + " (" + pais + ") - " + tipoAlojamiento;
+    }
+
 }
