e2f71d5c2d2974f396b302456a341d57d7ca0314
LujánEspinoza <usr4324@salesianos-lacuesta.net>
2026-04-20 10:11:48 +0100

    creacion clase "destino"

---
+++ src/net/salesianoscuesta/Destino.java
@@ -0,0 +1,8 @@
+package net.salesianoscuesta;
+
+public class Destino {
+        private String nombreDestino;
+        private String pais;
+        private String tipoAlojamiento;
+        private int puntuacion;
+}
